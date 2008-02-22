02dc5d0ff17e81b335154fda0070cf26ec099f3e
Ara Knaian <ara@phmgrid1.media.mit.edu>
2008-02-21 19:05:57 -0500

    Added dynamic updating to javaviewer.  Javaviewer checks the modification time and file size of the URL to the STL file once per second, and uses this to reload changes to the STL file when it changes on the server.  The result is one-click re-rendering with the web UI.  Also added a rendering and download progress bar.  (The rendering progress is not known numerically.)

--- javaviewer/javaviewer/Viewer.java
+++ javaviewer/javaviewer/Viewer.java
@@ -7,15 +7,21 @@ import java.nio.FloatBuffer;
 import java.util.Properties;
 import java.io.*;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 
 import javax.media.opengl.*;
+import javax.swing.JProgressBar;
+
 import com.sun.opengl.util.*;
 import java.lang.Math;
 
-/*  Kokompe Java Viewer 0.1
+/*  Kokompe Java Viewer 0.2
  *  By Ara Knaian, 2008
  *  
  *  Originally adapted from Gears demo by Brian Paul */
@@ -23,11 +29,24 @@ import java.lang.Math;
 public class Viewer implements GLEventListener, MouseListener, MouseMotionListener, MouseWheelListener {
 	
 	public static void main(String[] args) {
-    Frame frame = new Frame("KOKOMPE Viewer");
+   
+	String appname = System.getProperty("jnlp.title");	
+	if (appname == null) {
+		appname = "KOKOMPE Viewer";
+	}	
+	Frame frame = new Frame(appname);
     GLCanvas canvas = new GLCanvas();
     
+ 
+    progressBar = new JProgressBar(0, 100);
+    progressBar.setValue(0);
+    progressBar.setStringPainted(true);
+   
+    
     canvas.addGLEventListener(new Viewer());
-    frame.add(canvas);
+    frame.add(canvas, BorderLayout.CENTER);
+    
+    frame.add(progressBar, BorderLayout.PAGE_END);
     frame.setSize(600, 600);
     
     Point windowCorner = new Point(50,50);
@@ -51,6 +70,7 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
     animator.start();
   }
 
+  static private JProgressBar progressBar;
   private int gear1;
   private int prevMouseX, prevMouseY;
   private boolean mouseRButtonDown = false;
@@ -65,6 +85,11 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
   boolean panSet = false;
   Vector3 panVector = new Vector3();
   Vector3 lastPanVector = new Vector3();
+  URL stlfile;
+
+  Timer urlCheckTimer; 
+  long urlLastModified = 0;
+  boolean newData = true;
   
   private int readIntLittleEndian(DataInputStream in) throws IOException {
 	  
@@ -83,6 +108,48 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
     // Use debug pipeline
     // drawable.setGL(new DebugGL(drawable.getGL()));
 
+		// ************** SET UP TO DRAW WINDOW
+		
+	    GL gl = drawable.getGL();
+
+	    System.err.println("INIT GL IS: " + gl.getClass().getName());
+	    System.err.println("Chosen GLCapabilities: " + drawable.getChosenGLCapabilities());
+
+	    gl.setSwapInterval(1);
+	  
+	    // Position of the Light
+	    float pos[] = {60.0f, 60.0f, 120.0f, 0.0f };
+	    // Color of the Lighting 
+	    float ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f};
+	    float diffuseColor[] = {0.3f, 0.3f, 0.3f, 1.0f};
+	    float specularColor[] = {0.2f, 0.2f, 0.2f, 1.0f};
+	    // Color of the material
+	    // Four colors for an openGL object: ambient, diffuse, specular, and emission
+	    // specular and emission are fixed for the whole object, set here
+	    // ambient and diffuse are set by glColor
+	    float materialSpecularColor[] = {0.05f, 0.05f, 0.05f, 1.0f};  // set to 0 to diasable shadows
+	    float materialEmissionColor[] = {0.0f, 0.0f, 0.0f, 1.0f};     // set to nonzero to make object glow
+	    
+	    // ************* SET UP OPENGLSCENE LIGHTING ****************************
+	    
+	    gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, pos, 0);
+	    
+	    // my params
+	    gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambientColor, 0);
+	    gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseColor, 0);
+	    gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specularColor, 0);
+	    
+	    gl.glEnable(GL.GL_CULL_FACE);
+	    gl.glEnable(GL.GL_LIGHTING);
+	    gl.glEnable(GL.GL_LIGHT0);
+	    gl.glEnable(GL.GL_DEPTH_TEST);
+	    gl.glEnable(GL.GL_COLOR_MATERIAL);        
+	    
+	    gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, materialSpecularColor, 0);
+	    gl.glMaterialfv(GL.GL_FRONT, GL.GL_EMISSION, materialEmissionColor, 0);
+	    gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
+
+	    
 	// ******** INITIALIZE ROTATION MATRICIES FOR ARCBALL ROTATION
 		
 	// Set up the rotation storage matricies
@@ -112,163 +179,17 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
 	if (urltext == null) {
 		urltext = defaulturl;
 	}
-
-	// ********** DOWNLOAD STL FILE *********************************
 	
 	try {
+		stlfile = new URL(urltext);
+	}
+ 		catch (MalformedURLException e) {
+		System.err.println("Malformed URL!");
+		System.exit(0);
+	}
 		
-	URL stlfile = new URL(urltext);
-	URLConnection filestream = stlfile.openConnection();
-	DataInputStream in = new DataInputStream(filestream.getInputStream());   
-
-	// Skip past the header
-	byte[] header;
-	header = new byte[80];
-	in.read(header, 0, 80);    
-	
-	for(int m=0;m<80;m++) {
-		Byte firstchar = new Byte(header[m]);
-		System.err.print(firstchar.toString() + " ");
-	}
-	System.err.println();
-	
-	// Read in the number of facets
-	int numStlFacets = 0;
-	numStlFacets = readIntLittleEndian(in);
-	System.err.println("STL file in URL contains " + numStlFacets + " facets.");
-	
-	// Read the actual facet data
-	//	 facet data is 12 floats plus a short (4*12 + 2 = 50 bytes/facet)
-	
-	int bytesToRead = numStlFacets*50;
-	byte [] facetdata = new byte[bytesToRead];  
-	int bytesRead = 0;
-	
-	while (bytesRead < bytesToRead) {
-		bytesRead += in.read(facetdata, bytesRead, bytesToRead - bytesRead);
-		System.err.println("Bytes Read: " + bytesRead);
-	}
-	
-	// Done reading URL - close connection
-    in.close();	
-
-    // *************** REFORMAT DATA FOR OPENGL ***************************
-    // Little Endian vs. Big Endian byte ordering, etc.
-    
-    float[] normalArray = new float[numStlFacets*9];
-    float[] vertexArray = new float[numStlFacets*9];
-
-	// Now reformat data into float arrays
-    
-    int byteCtr = 0;
-    int normalCtr = 0;
-    int vertexCtr = 0;
-    
-    for (int facet = 0; facet < numStlFacets; facet++) {
-    
-      // Get the 3 floats defining the facet normal 	
-      for (int normalFloat = 0; normalFloat < 3; normalFloat++) {	   	
-    	  int accum = 0;
-    	  for ( int shiftBy=0; shiftBy<32; shiftBy+=8 ) {
-    		  accum |= ( facetdata[byteCtr] & 0xff ) << shiftBy;
-    		  byteCtr++;
-	      }
-    	  normalArray[normalCtr++] = (float)Float.intBitsToFloat( accum );
-      }
-      // Duplicate normal twice
-      for(int k=0; k<6; k++) {
-    		  normalArray[normalCtr] = normalArray[normalCtr-3];
-    		  normalCtr++;
-      }
-      
-      //    Get the 9 floats defining the vertex coordinates 	
-      for (int vertexFloat = 0; vertexFloat < 9; vertexFloat++) {	   	
-    	  int accum = 0;
-    	  for ( int shiftBy=0; shiftBy<32; shiftBy+=8 ) {
-    		  accum |= ( facetdata[byteCtr++] & 0xff ) << shiftBy;
-	      }
-    	  vertexArray[vertexCtr++] = ((float)Float.intBitsToFloat( accum )) - 10.0f;
-      }	 
-      
-      // Skip attribute byte count
-      byteCtr += 2;
-      
-    }
-	  
-    // ************** WRAP FLOAT ARRAYS IN FLOATBUFFER OBJECTS FOR JOGL
-    
-    ByteBuffer underlyingVertexBuffer = ByteBuffer.allocateDirect(numStlFacets*9*4).order(ByteOrder.nativeOrder());
-    FloatBuffer vertexBuffer = 	underlyingVertexBuffer.asFloatBuffer();   
-    vertexBuffer.put(vertexArray, 0, numStlFacets*9);
-    vertexBuffer.position(0);
-    
-    ByteBuffer underlyingNormalBuffer = ByteBuffer.allocateDirect(numStlFacets*9*4).order(ByteOrder.nativeOrder());
-    FloatBuffer normalBuffer = 	underlyingNormalBuffer.asFloatBuffer();   
-    normalBuffer.put(normalArray, 0, numStlFacets*9);
-    normalBuffer.position(0);
-    
-      
-	// ************** SET UP TO DRAW WINDOW
-	
-    GL gl = drawable.getGL();
-
-    System.err.println("INIT GL IS: " + gl.getClass().getName());
-    System.err.println("Chosen GLCapabilities: " + drawable.getChosenGLCapabilities());
-
-    gl.setSwapInterval(1);
-
-    // Position of the Light
-    float pos[] = {60.0f, 60.0f, 120.0f, 0.0f };
-    // Color of the Lighting 
-    float ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f};
-    float diffuseColor[] = {0.3f, 0.3f, 0.3f, 1.0f};
-    float specularColor[] = {0.2f, 0.2f, 0.2f, 1.0f};
-    // Color of the material
-    // Four colors for an openGL object: ambient, diffuse, specular, and emission
-    // specular and emission are fixed for the whole object, set here
-    // ambient and diffuse are set by glColor
-    float materialSpecularColor[] = {0.05f, 0.05f, 0.05f, 1.0f};  // set to 0 to diasable shadows
-    float materialEmissionColor[] = {0.0f, 0.0f, 0.0f, 1.0f};     // set to nonzero to make object glow
-    
-    // ************* SET UP OPENGLSCENE LIGHTING ****************************
-    
-    gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, pos, 0);
-    
-    // my params
-    gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambientColor, 0);
-    gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuseColor, 0);
-    gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, specularColor, 0);
-    
-    gl.glEnable(GL.GL_CULL_FACE);
-    gl.glEnable(GL.GL_LIGHTING);
-    gl.glEnable(GL.GL_LIGHT0);
-    gl.glEnable(GL.GL_DEPTH_TEST);
-    gl.glEnable(GL.GL_COLOR_MATERIAL);        
-            
-    // ************ CREATE A DISPLAY LIST CONTAINING THE STL OBJECT *********
-    
-    gear1 = gl.glGenLists(1);
-    gl.glNewList(gear1, GL.GL_COMPILE);
-  //  gl.glScalef(6.0f, 6.0f, 6.0f);
-    
-    gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, materialSpecularColor, 0);
-    gl.glMaterialfv(GL.GL_FRONT, GL.GL_EMISSION, materialEmissionColor, 0);
-    gl.glColorMaterial(GL.GL_FRONT, GL.GL_AMBIENT_AND_DIFFUSE);
-    
-    // Red object --- change here to change color - RGB
-    // (or could render color with a color array)
-    gl.glColor3f(1.0f, 0.0f, 0.0f);
-    
-    // Create vertex arrays
-    gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
-    gl.glEnableClientState(GL.GL_NORMAL_ARRAY); 
-    gl.glVertexPointer(3, GL.GL_FLOAT, 0, vertexBuffer);
-    gl.glNormalPointer(GL.GL_FLOAT, 0, normalBuffer);
-    
-    // Draw Triangles
-    gl.glDrawArrays(GL.GL_TRIANGLES, 0, numStlFacets*3);
-    
-    gl.glEndList();    
+   if(updateModel(gl))
+	   newData = false;
     
     // **************** SET UP MOUSE LISTENERS *****************
     
@@ -276,26 +197,238 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
     drawable.addMouseMotionListener(this);
     drawable.addMouseWheelListener(this);
     
-	}
-	catch (MalformedURLException e) {
-		System.err.println("Malformed URL!");
-		
-	}
-	catch (IOException e) {;
-		System.err.println("I/O Exception!");	
-	}
+	
+    urlCheckTimer = new Timer();
+    urlCheckTimer.scheduleAtFixedRate(new TimerTask() {
+    	public void run() {
+    		try {
+    			URLConnection filestream2 = stlfile.openConnection();
+    			filestream2.connect();
+    			System.err.println(" Last modified 1: " + filestream2.getLastModified() + " Length: " + filestream2.getContentLength());
+    			if ((filestream2.getLastModified() > urlLastModified) && (newData == false)) {
+    				// Let the user know rendering has started
+    				progressBar.setValue(0);
+    				progressBar.setIndeterminate(true);
+    				progressBar.setString("Rendering Model...");
+    				if (filestream2.getContentLength() >= 84) {
+    				
+    				newData = true;				// file must be long enough to be valid STL to start reading
+    				}
+    			}
+    		
+    	}
+    		catch (IOException e) {
+    			System.out.println(" I/O Exception trying to re-read modification time!");	
+    		}
+    	} 	
+    }, (long)1000, (long)1000);
+    
+    
+    
+    
+
   }
   
+  public boolean updateModel(GL gl) {
+	  int numStlFacets = 0;
+	  byte [] facetdata = new byte[0];
+	  
+	  try {
+			
+		  	
+		  	URLConnection filestream;
+		  
+			filestream = stlfile.openConnection();
+
+			urlLastModified = filestream.getLastModified();
+			int urlContentLength = filestream.getContentLength();
+			
+			DataInputStream in = new DataInputStream(filestream.getInputStream());   
+
+			// Skip past the header
+			byte[] header;
+			header = new byte[80];
+			in.read(header, 0, 80);    
+			
+			for(int m=0;m<80;m++) {
+				Byte firstchar = new Byte(header[m]);
+				System.err.print(firstchar.toString() + " ");
+			}
+			System.err.println();
+			
+			// Read in the number of facets
+			numStlFacets = 0;
+			numStlFacets = readIntLittleEndian(in);
+			System.err.println("STL file in URL contains " + numStlFacets + " facets.");
+			
+			// Read the actual facet data
+			//	 facet data is 12 floats plus a short (4*12 + 2 = 50 bytes/facet)
+			
+			int bytesToRead = numStlFacets*50;
+			
+			if (urlContentLength < (bytesToRead + 84)) {
+				// file is not yet finished writing.
+				// return false, which will result in trying again in a moment
+				return(false);
+			}
+			
+			// OK, we have a valid file!!! set up progress bar
+			progressBar.setMinimum(0);
+			progressBar.setMaximum(bytesToRead);
+			progressBar.setValue(0);
+			progressBar.setIndeterminate(false);
+			Rectangle progressRect = progressBar.getBounds();
+			progressRect.x = 0;
+			progressRect.y = 0;
+			
+			
+			facetdata = new byte[bytesToRead];  
+			int bytesRead = 0;
+			int bytesThisRead = 0;
+			
+			while ((bytesRead < bytesToRead) && (bytesThisRead != -1)) {
+				bytesThisRead = in.read(facetdata, bytesRead, bytesToRead - bytesRead);
+				if (bytesThisRead > 0) {
+					bytesRead += bytesThisRead;
+					progressBar.setValue(bytesRead);
+					progressBar.setString("Downloading Model...");
+					progressBar.paintImmediately(progressRect);
+					
+				}
+				System.err.println("Bytes Read: " + bytesRead);
+			}
+
+			
+			// Done reading URL - close connection
+		    in.close();	
+
+		    if (bytesThisRead == -1) {
+		    	System.err.println("File ended unexpectedly while downloading");
+		    	return(false);
+		    }		    
+		  }
+		catch (IOException e) {
+			System.err.println("I/O Exception reading data from server.");
+			return(false);
+		}
+	
+		progressBar.setString("Displaying Model...");
+		
+		if (numStlFacets > 0) {    
+		    
+		    // *************** REFORMAT DATA FOR OPENGL ***************************
+		    // Little Endian vs. Big Endian byte ordering, etc.
+		    
+		    float[] normalArray = new float[numStlFacets*9];
+		    float[] vertexArray = new float[numStlFacets*9];
+
+			// Now reformat data into float arrays
+		    
+		    int byteCtr = 0;
+		    int normalCtr = 0;
+		    int vertexCtr = 0;
+		    
+		    for (int facet = 0; facet < numStlFacets; facet++) {
+		    
+		      // Get the 3 floats defining the facet normal 	
+		      for (int normalFloat = 0; normalFloat < 3; normalFloat++) {	   	
+		    	  int accum = 0;
+		    	  for ( int shiftBy=0; shiftBy<32; shiftBy+=8 ) {
+		    		  accum |= ( facetdata[byteCtr] & 0xff ) << shiftBy;
+		    		  byteCtr++;
+			      }
+		    	  normalArray[normalCtr++] = (float)Float.intBitsToFloat( accum );
+		      }
+		      // Duplicate normal twice
+		      for(int k=0; k<6; k++) {
+		    		  normalArray[normalCtr] = normalArray[normalCtr-3];
+		    		  normalCtr++;
+		      }
+		      
+		      //    Get the 9 floats defining the vertex coordinates 	
+		      for (int vertexFloat = 0; vertexFloat < 9; vertexFloat++) {	   	
+		    	  int accum = 0;
+		    	  for ( int shiftBy=0; shiftBy<32; shiftBy+=8 ) {
+		    		  accum |= ( facetdata[byteCtr++] & 0xff ) << shiftBy;
+			      }
+		    	  vertexArray[vertexCtr++] = ((float)Float.intBitsToFloat( accum )) - 10.0f;
+		      }	 
+		      
+		      // Skip attribute byte count
+		      byteCtr += 2;
+		      
+		    }
+			  
+		    // ************** WRAP FLOAT ARRAYS IN FLOATBUFFER OBJECTS FOR JOGL
+		    
+		    ByteBuffer underlyingVertexBuffer = ByteBuffer.allocateDirect(numStlFacets*9*4).order(ByteOrder.nativeOrder());
+		    FloatBuffer vertexBuffer = 	underlyingVertexBuffer.asFloatBuffer();   
+		    vertexBuffer.put(vertexArray, 0, numStlFacets*9);
+		    vertexBuffer.position(0);
+		    
+		    ByteBuffer underlyingNormalBuffer = ByteBuffer.allocateDirect(numStlFacets*9*4).order(ByteOrder.nativeOrder());
+		    FloatBuffer normalBuffer = 	underlyingNormalBuffer.asFloatBuffer();   
+		    normalBuffer.put(normalArray, 0, numStlFacets*9);
+		    normalBuffer.position(0);
+		    
+		     
+		   
+		            
+		    // ************ CREATE A DISPLAY LIST CONTAINING THE STL OBJECT *********
+		    
+		    gear1 = gl.glGenLists(1);
+		    gl.glNewList(gear1, GL.GL_COMPILE);
+		  //  gl.glScalef(6.0f, 6.0f, 6.0f);
+		    
+	    
+		    // Red object --- change here to change color - RGB
+		    // (or could render color with a color array)
+		    gl.glColor3f(1.0f, 0.0f, 0.0f);
+		    
+		    // Create vertex arrays
+		    gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
+		    gl.glEnableClientState(GL.GL_NORMAL_ARRAY); 
+		    gl.glVertexPointer(3, GL.GL_FLOAT, 0, vertexBuffer);
+		    gl.glNormalPointer(GL.GL_FLOAT, 0, normalBuffer);
+		    
+		    // Draw Triangles
+		    gl.glDrawArrays(GL.GL_TRIANGLES, 0, numStlFacets*3);
+		    
+		    gl.glEndList();   
+	  
+		}
+		
+		else {
+			// display blank screen on valid STL file with no facets
+			 gear1 = gl.glGenLists(1);
+			 gl.glNewList(gear1, GL.GL_COMPILE);
+			 gl.glEndList();
+			
+		}
+		progressBar.setString("");
+		
+		return(true);
+	}
+		
+  
+  
+  
+  
   public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-    GL gl = drawable.getGL();
     windowAspectRatio = (float)height / (float)width;			
   }
 
   public void display(GLAutoDrawable drawable) {
     	
 	float viewDistance = 6.0f;
-	  
-    GL gl = drawable.getGL();
+
+    GL gl = drawable.getGL();  
+    
+    if (newData){
+    	if(updateModel(gl));
+    	newData = false;
+    }
+    
     if ((drawable instanceof GLJPanel) &&
         !((GLJPanel) drawable).isOpaque() &&
         ((GLJPanel) drawable).shouldPreserveColorBufferIfTranslucent()) {
@@ -336,7 +469,8 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
     
     // Draw the object
     gl.glCallList(gear1);
-   // gl.glPopMatrix();       
+   // gl.glPopMatrix();    
+	
   }
   
   // Methods required for the implementation of MouseListener
@@ -430,5 +564,6 @@ public class Viewer implements GLEventListener, MouseListener, MouseMotionListen
     	  zoomFactor /= 1.1;
   
   }
+  
 }
 
